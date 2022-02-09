f5cff920c90c41bd56182d87d74cf56089f6d204
98kimjh <98kimjh@gmail.com>
2022-02-09 17:25:19 +0900

    [jblog04] app MyBatis Config (xml -> java) / Mixing to AppConfig.java

--- jblog04/src/main/java/com/poscoict/jblog/config/AppConfig.java
+++ jblog04/src/main/java/com/poscoict/jblog/config/AppConfig.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import com.poscoict.config.app.DBConfig;
+import com.poscoict.config.app.MyBatisConfig;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({ "com.poscoict.jblog.repository", "com.poscoict.jblog.service", "com.poscoict.jblog.aspect" })
-@Import({ DBConfig.class })
+@Import({ DBConfig.class, MyBatisConfig.class })
 public class AppConfig {
 
 }
