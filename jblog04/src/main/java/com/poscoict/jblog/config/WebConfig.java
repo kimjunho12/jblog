1cb2b10a609af775ced40b57e74475bc12dfa248
98kimjh <98kimjh@gmail.com>
2022-02-09 17:45:15 +0900

    [jblog04] Web Config (xml -> java) /w WebConfig.java

---
+++ jblog04/src/main/java/com/poscoict/jblog/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.poscoict.jblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.poscoict.jblog.interceptor.BlogInterceptor;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan({ "com.poscoict.jblog.controller", "com.poscoict.jblog.exception" })
+@Import({})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	// Blog Interceptor
+	@Bean
+	public HandlerInterceptor blogInterceptor() {
+		return new BlogInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(blogInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/")
+			.excludePathPatterns("/main/**")
+			.excludePathPatterns("/user/**")
+			.excludePathPatterns("/assets/**")
+			.excludePathPatterns("/images/**");
+	}
+
+}
